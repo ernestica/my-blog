760fb10a1c17d69173267472f7844a8697ec9250
ernestica <55663186+ernestica@users.noreply.github.com>
2020-04-17 19:17:51 -0400

    First Commit

---
+++ src/build/precache-manifest.10ad7774f26cbf1aef290dc9c0f7d337.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0d7355a6994963f738ef8f16278867",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc3786d8714077791082",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "968c29e76d68c12f36ac",
+    "url": "/static/css/main.5291dc58.chunk.css"
+  },
+  {
+    "revision": "fc3786d8714077791082",
+    "url": "/static/js/2.dab70367.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dab70367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "968c29e76d68c12f36ac",
+    "url": "/static/js/main.c0aebd9b.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  },
+  {
+    "revision": "e64da3363aa030c5cf9b2c15abfe591f",
+    "url": "/static/media/crave-and-eat-header.e64da336.png"
+  },
+  {
+    "revision": "86ce674c6939884725dc2e825c3bad5b",
+    "url": "/static/media/crave-and-eat-mainidea.86ce674c.png"
+  },
+  {
+    "revision": "466e8ecc985c7fc7514ecabbab1959b5",
+    "url": "/static/media/diversity-header.466e8ecc.png"
+  },
+  {
+    "revision": "453c483b639301274b691ee837589f79",
+    "url": "/static/media/diversity-process.453c483b.png"
+  },
+  {
+    "revision": "e45e067a1d73c172c734552736f4d82c",
+    "url": "/static/media/happypaws-sketches.e45e067a.png"
+  },
+  {
+    "revision": "0350b9f445f5cc77d1f6de5916bc6bdc",
+    "url": "/static/media/header-happypaws.0350b9f4.png"
+  },
+  {
+    "revision": "f016cfba318e1259daae3ebf7e8e35c5",
+    "url": "/static/media/hero.f016cfba.png"
+  },
+  {
+    "revision": "df638e92859f408b93ba71ebec82ecdf",
+    "url": "/static/media/logo.df638e92.png"
+  },
+  {
+    "revision": "5e64e10eba39a62d603c84ee88215d19",
+    "url": "/static/media/styletile_crave-and-eat.5e64e10e.png"
+  },
+  {
+    "revision": "034e76d04f07cdbf4e7a24e5ac21a511",
+    "url": "/static/media/styletile_diversityhousing.034e76d0.png"
+  },
+  {
+    "revision": "fd876b70d15922dbde8da965cff82063",
+    "url": "/static/media/styletile_happypaws.fd876b70.png"
+  }
+]);
